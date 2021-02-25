6cfa1567b7259562560ea79543b75420c1da75c3
aleksej <martyanov.alexei@gmail.com>
2021-02-25 11:36:03 +0300

    Add a project

---
+++ testTask/src/test/java/com/example/testTask/TestTaskControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.testTask;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTaskControllerTest {
+
+    private
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void sayHello() {
+
+    }
+
+    @Test
+    void checkCurrency() {
+    }
+}
